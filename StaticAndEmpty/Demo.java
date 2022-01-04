8c5f7a4825dab2d5babc55df9b06a243313849e2
VPACHCHU <vpachchu27@gmail.com>
2022-01-04 22:57:37 +0530

    first commit on Static and Empty blocks

---
+++ StaticAndEmpty/Demo.java
@@ -0,0 +1,26 @@
+package com.StaticAndEmpty;
+
+
+public class Demo {
+
+    static {
+        System.out.println("Hi! I'm static01 block");
+    }
+
+    Demo()
+    {
+        System.out.println("Hi! I'm a constructor");
+    }
+
+
+    {
+        System.out.println("Hi! I'm empty block");
+
+    }
+
+    static {
+        System.out.println("Hi! I'm static02 block");
+    }
+
+
+}
